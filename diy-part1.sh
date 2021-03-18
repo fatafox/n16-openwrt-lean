709670111944d7953c522c536dfbd51f67c068cc
fatafox <80817169+fatafox@users.noreply.github.com>
2021-03-18 10:12:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/AdguardTeam/AdGuardHome.git package/AdGuardHome
+git clone https://github.com/fw876/helloworld.git package/helloworld
